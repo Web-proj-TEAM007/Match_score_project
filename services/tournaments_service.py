f6eb3a6a8c70b182892fefceab45b1b75bccb1cd
GeorgiStefanov99 <georgi.stst@abv.bg>
2023-11-08 15:14:35 +0300

    Now generate_knockout_scheme work as needed

--- services/tournaments_service.py
+++ services/tournaments_service.py
@@ -1,7 +1,6 @@
-import random
-
 from data.database import read_query, update_query, insert_query
 from data.models import Tournament, Player
+import random
 
 
 def get_all_tournaments(title, tour_format):
@@ -22,35 +21,34 @@ def get_all_tournaments(title, tour_format):
 
 
 def generate_knockout_scheme(players):
-    scheme_format = ''
+    match_players = []
     players_count = len(players)
+    if players_count == 0:
+        return match_players
     if players_count % 2 != 0:
-        return None
+        raise ValueError('Players must be even numbers')
     if players_count == 2:
-        scheme_format = 'final'
-    elif players_count == 4:
-        scheme_format = 'semi-final'
-    elif players_count == 6:
-        scheme_format = 'quarterfinals'
-    elif players_count == 8:
-        scheme_format = 'eight-finals'
-    elif players_count > 8:
-        return 'Poveche nedavam'
-    if players_count > 2:
-        first_match_player1 = random.choice(players)
-        players.remove(first_match_player1)
-        first_match_player2 = random.choice(players)
-        players.remove(first_match_player2)
-        second_match_player1 = random.choice(players)
-        players.remove(second_match_player1)
-        second_match_player2 = random.choice(players)
-        players.remove(second_match_player2)
-        first_match = {'first_player': first_match_player1, 'second_player': first_match_player2}
-        second_match = {'first_player': second_match_player1, 'second_player': second_match_player2}
-    else:
-        last_match_player1 = random.choice(players)
-        last_match_player2 = random.choice(players)
-        last_match = {'first_player': last_match_player1, 'second_player': last_match_player2}
-        return scheme_format, last_match
-    return scheme_format, first_match, second_match
+        return 'final', {'first_player': players[0], 'second_player': players[1]}
+    first_match_player1 = random.choice(players)
+    players.remove(first_match_player1)
+    first_match_player2 = random.choice(players)
+    players.remove(first_match_player2)
+    second_match_player1 = random.choice(players)
+    players.remove(second_match_player1)
+    second_match_player2 = random.choice(players)
+    players.remove(second_match_player2)
+    match_players.append({'first_player': first_match_player1, 'second_player': first_match_player2})
+    match_players.append({'first_player': first_match_player1, 'second_player': first_match_player2})
+    match_players.extend(generate_knockout_scheme(players))
+    return match_players
 
+
+def get_scheme_format(players_count):
+    if players_count == 4:
+        return 'semi-final'
+    elif players_count == 6:
+        return 'quarterfinals'
+    elif players_count == 8:
+        return 'eight-finals'
+    else:
+        return 'Poveche nedavam'
