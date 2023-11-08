dcd82743fe54b133226b33537c2fa41f2724e80f
GeorgiStefanov99 <georgi.stst@abv.bg>
2023-11-08 15:13:49 +0300

    implemented logic for getting all players by scheme, further  ti create a match by the result of the 'knockout'

--- routers/tournaments.py
+++ routers/tournaments.py
@@ -25,13 +25,23 @@ def get_all_tournaments(title: str = Query(None, description="Get tournament by 
 def create_tournament(tournament: TournamentCreateModel, token: Depends(JWTBearer())):
     """Only director can create tournaments"""
     if tournament.tour_format == "Knockout":
-        # check if each player is already existing, if not create one and link it to profile
+        # ---------- check if each player is already existing, if not create one and link it to profile --------
+        # ------------------------------- waiting for player implementation ------------------------------------
+        # players = []
         # for player_name in tournament.participants:
         #     if not player_service.exists(player_name):
         #         player = player_service.create_player(player_name)
         #         player_service.create_profile_for_player(player)
-        # if len(tournament.participants) > 2:
-        #     schema, first_match, second_match = tournaments_service.generate_knockout_scheme(tournament.participants)
+        #         players.append(player)
+        # schema = tournaments_service.get_scheme_format(len(players))
+        #  --------- here we randomly assign every player to a match depending on the schema ----------
+        # result = tournaments_service.generate_knockout_scheme(players)
+        # RETURN OF SCHEMA AND RESULT:
+        #   eight-finals
+        #   [{'first_player': 'Player5', 'second_player': 'Player3'}, {'first_player': 'Player5','second_player':
+        #   'Player3'},{'first_player': 'Player7', 'second_player': 'Player6'},
+        #   {'first_player': 'Player7', 'second_player': 'Player6'}]
+        # ------------- further implementation of creating these matches ---------
         pass
     elif tournament.tour_format == "League":
         pass
