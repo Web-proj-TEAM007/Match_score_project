a36861b7d01d264a248ca252e7be231576c6d2b1
Angel0v22w <velislavangelov19@gmail.com>
2023-11-09 17:29:29 +0200

    get_user_from_token function

--- authentication/auth.py
+++ authentication/auth.py
@@ -1,10 +1,10 @@
-# from common.exceptions import Unauthorized
-# from data.models import Player
-# from services.player_service import find_by_username
-# from authentication.jwt_handler import decode_jwt
+from common.exceptions import Unauthorized
+from data.models import User
+from services.user_service import email_exists
+from authentication.jwt_handler import decode_jwt
 
-# def get_user_from_token(token: str) -> User:
-#     decoded_token = decode_jwt(token)
-#     username = decoded_token.get("username")
-#     user = find_by_username(username)
-#     return user
+def get_user_from_token(token: str) -> User:
+    decoded_token = decode_jwt(token)
+    email = decoded_token.get("email")
+    user = email_exists(email)
+    return user
