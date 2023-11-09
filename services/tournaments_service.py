7b155b1c37d2b98f601d02a650d013f2e9b02218
GeorgiStefanov99 <georgi.stst@abv.bg>
2023-11-09 10:28:42 +0300

    created get_tournament_by_id, get_tournament_participants(which most likely be moved from there), in progress function for managing the tournament, rename generate schema(both league and knockout have same game model)

--- services/tournaments_service.py
+++ services/tournaments_service.py
@@ -20,7 +20,36 @@ def get_all_tournaments(title, tour_format):
     return tournaments
 
 
-def generate_knockout_scheme(players):
+def get_tournament_by_id(tour_id):
+    data = read_query('SELECT * FROM tournaments WHERE id = ?', (tour_id,))
+    tournament = next((Tournament.from_query_result(*row) for row in data), None)
+    return tournament
+
+
+# ------ this will be moved to a player_service most likely but for now is here to test tournaments get by id -----
+def get_tournament_participants(tour_id):
+    data = read_query('SELECT player_profile_id FROM tournaments_has_players_profile '
+                      'WHERE tournament_id = ?', (tour_id,))
+    participants = ((Player.from_query_result(*row) for row in data), None)
+    return participants
+
+
+def manage_event(tournament, new_date, change_participants):
+    if new_date:
+        # maybe start_date of the tournament need to be added to the db
+        #   update_query("UPDATE tournaments SET start_date = ? WHERE id = ?", new_date, tournament.id)
+        # IMPORTANT: after we change the tournament date make sure to move match date if needed
+        tournament.start_date = new_date
+    if change_participants:
+        old, new = change_participants
+        date = update_query(
+            "UPDATE tournaments_has_player_profiles SET player_profile_id = ? WHERE tournament_id = ? "
+            "AND player_profile_id = ?",
+            new.id, tournament.id, old.id
+        )
+
+
+def generate_game_schema(players):
     match_players = []
     players_count = len(players)
     if players_count == 0:
@@ -39,7 +68,7 @@ def generate_knockout_scheme(players):
     players.remove(second_match_player2)
     match_players.append({'first_player': first_match_player1, 'second_player': first_match_player2})
     match_players.append({'first_player': first_match_player1, 'second_player': first_match_player2})
-    match_players.extend(generate_knockout_scheme(players))
+    match_players.extend(generate_game_schema(players))
     return match_players
 
 
