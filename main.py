0988205b9e3504d18fe2787c8d9af73a6af65a94
Angel0v22w <velislavangelov19@gmail.com>
2023-11-09 17:28:57 +0200

    User router

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from routers.user import users_router
 import uvicorn
 
 
+
 app = FastAPI(debug=True)
+app.include_router(users_router)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', port=8000, reload=True)
