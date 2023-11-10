2d5d5514ca456c5ac508141be31cf96e92c2cf60
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-11-11 00:43:37 +0200

    match_serv: + def create_match, + def get_particiapants_ids

--- services/match_service.py
+++ services/match_service.py
@@ -1,9 +1,20 @@
 from data.database import read_query, update_query, insert_query
-from data.models import Tournament, Player, Match
+from data.models import Tournament, Player, Match, MatchTournResponseMod
 
 
 # from services.tournaments_service import get_tournament_title_by_id <-- You can get it from get_tournament_by_id and
 # then tournament.title
+
+# class Tournament(BaseModel):
+#     id: Optional[int] = None
+#     title: str
+#     tour_format: str
+#     prize: int
+#     schema: str | None = None
+#     match_format: str | None = None
+#     participants: list[str] | None = None
+#     matches: Optional[Match] | None = None
+#     start_date: Optional[datetime] = None
 
 # class Match(BaseModel):
 #     id: int | None = None
@@ -12,19 +23,48 @@ from data.models import Tournament, Player, Match
 #     tourn_id: int
 
 # needed: format, date, tourn_id
-def create_match(match: Match) -> Match:
-    generated_id = insert_query('''INSERT INTO matches(format, date, tournament_id) VALUES(?,?,?)''',
-                                (match.format, match.date, match.tourn_id))
-    
-    match.id = generated_id
-    return match
+def create_match(tournament: Tournament) -> list[MatchTournResponseMod]:
 
+    matches = []
+    participants = tournament.participants[::-1]
+    players_ids = get_participants_ids(participants)
+    n = len(players_ids) / 2
+    for _ in range(int(n)):
+        match_id = insert_query('''INSERT INTO matches(format, date, tournament_id)
+                              VALUES(?,?,?)''',(tournament.match_format, tournament.start_date, tournament.id))
+        
+        player_1 = players_ids[-1]
+        player_2 = players_ids[-2]
+        players_ids.pop()
+        players_ids.pop()
 
-def insert_players_into_matches(match_id: int, player1_id: int, player2_id: int):
-    
-    insert_query('''INSERT INTO matches_has_players_profiles(matches_id, player_profile_id, score)
-                 VALUES(?,?,?)'''(match_id, player1_id, player2_id))
-    
+        insert_query('''INSERT INTO matches_has_players_profiles(matches_id, player_profile_id, score)
+                    VALUES(?,?,0)''', (match_id, player_1))
+        insert_query('''INSERT INTO matches_has_players_profiles(matches_id, player_profile_id, score)
+                    VALUES(?,?,0)''', (match_id, player_2))
+        
+        player_1_name = participants[-1]
+        player_2_name = participants[-2]
+        participants.pop()
+        participants.pop()
+        this_match = MatchTournResponseMod(id=match_id, 
+                                           player_1=player_1_name, 
+                                           player_2=player_2_name, 
+                                           date='Not set yet')
+        matches.append(this_match)
+
+    return matches
+            
+
+def get_participants_ids(participants: list[str]) -> list[int]:
+
+    players_ids = []
+    for player in participants:
+
+        data = read_query('''SELECT id FROM players_profiles
+                        WHERE full_name = ?''', (player,))
+        players_ids.append(data[0][0])
+    return players_ids
 
 def get_matches_for_tournament(tournament_id: int):
     # ---- To Shahin: Will need this function to properly return tournaments
