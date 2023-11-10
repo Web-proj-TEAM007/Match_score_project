e3476bdaaa2a3be19a8f52631313b42782b43842
GeorgiStefanov99 <georgi.stst@abv.bg>
2023-11-10 11:36:13 +0300

    commented because it was raising errors because get_all_matches was not currently in use

--- routers/matches.py
+++ routers/matches.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Query, Depends, Path
 from datetime import datetime
-from services import tournaments_service, match_serivce
+from services import tournaments_service, match_service
 from common.exceptions import NoContent, NotFound, BadRequest
 from authentication.jwt_bearer import JWTBearer
 # from authentication.auth import get_user_from_token
@@ -14,10 +14,10 @@ def get_all_matches(sort_by_date: datetime = Query(None, description='Filter by 
                                                                      'YYYY-MM-DDTHH:MM:SS eg. 2023-10-21T00:00:00 :'),
                     sort_by_tournament_id: int = Query(default=None, description='Search by topic id')):
     """You can get all existing matches, optional filter features"""
-    matches = match_serivce.get_all_matches(sort_by_date, sort_by_tournament_id)
-    if not matches:
-        raise NotFound(content='No matches')
-    return matches
+    # matches = match_service.get_all_matches(sort_by_date, sort_by_tournament_id)
+    # if not matches:
+    #     raise NotFound(content='No matches')
+    # return matches
 
 
 @match_router.get('/{match_id}')
