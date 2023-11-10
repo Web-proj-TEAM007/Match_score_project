fd23b3ef0efc80169c15b3f8a142b168c19cf299
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-11-10 18:21:52 +0200

    user_service: fixed query

--- services/user_service.py
+++ services/user_service.py
@@ -58,7 +58,7 @@ def player_profile_exists(full_name) -> bool:
 
 
 def create_player_statistic(player: Player):
-    data = insert_query("INSERT INTO players_statistics(players_profiles_id) VALUES(?)", (player.id,))
+    data = insert_query("INSERT INTO players_statistics(player_profile_id) VALUES(?)", (player.id,))
     return data
 
 
