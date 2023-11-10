8e6dfe8d5c1a28051abe8861e53c566739584a12
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-11-10 17:54:30 +0200

    match_serv: + create_match, + def insert_players_into_matches

--- services/match_service.py
+++ services/match_service.py
@@ -5,14 +5,26 @@ from data.models import Tournament, Player, Match
 # from services.tournaments_service import get_tournament_title_by_id <-- You can get it from get_tournament_by_id and
 # then tournament.title
 
+# class Match(BaseModel):
+#     id: int | None = None
+#     format: str
+#     date: datetime | str = 'not set yet'
+#     tourn_id: int
 
 # needed: format, date, tourn_id
-def create_match(tournament: Tournament) -> Match:
-    generated_id = insert_query('''INSERT INTO matches(tournament_id, format) VALUES(?, ?)''',
-                                (tournament.id, tournament.match_format))
-    match = Match(id=generated_id, format=tournament.match_format, tourn_id=tournament.id)
+def create_match(match: Match) -> Match:
+    generated_id = insert_query('''INSERT INTO matches(format, date, tournament_id) VALUES(?,?,?)''',
+                                (match.format, match.date, match.tourn_id))
+    
+    match.id = generated_id
     return match
 
+
+def insert_players_into_matches(match_id: int, player1_id: int, player2_id: int):
+    
+    insert_query('''INSERT INTO matches_has_players_profiles(matches_id, player_profile_id, score)
+                 VALUES(?,?,?)'''(match_id, player1_id, player2_id))
+    
 
 def get_matches_for_tournament(tournament_id: int):
     # ---- To Shahin: Will need this function to properly return tournaments
