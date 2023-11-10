b7fe03ea5dd8b9955e1854629b962ee8ac379b71
GeorgiStefanov99 <georgi.stst@abv.bg>
2023-11-10 14:52:52 +0300

    adjusted create_tournament to work, change the generate_game_schema, tournament_exists now is searched by title

--- services/tournaments_service.py
+++ services/tournaments_service.py
@@ -1,7 +1,9 @@
 from data.database import read_query, update_query, insert_query
-from data.models import Tournament, Player, TournamentCreateModel
+from data.models import Tournament, Player
 import random
 from common.validators import tournament_format_validator
+from common.exceptions import BadRequest
+from fastapi import Response
 
 
 def get_all_tournaments(title, tour_format):
@@ -27,9 +29,9 @@ def get_tournament_by_id(tour_id: int):
     return tournament
 
 
-def create_tournament(tournament: TournamentCreateModel):
-    generated_id = insert_query("INSERT INTO tournaments(format, title, prize), VALUES(?, ?, ?)",
-                                (tournament.tour_format, tournament.title, tournament.prize))
+def create_tournament(tournament: Tournament):
+    generated_id = generated_id = insert_query("INSERT INTO tournaments (format, title, prize) VALUES (?, ?, ?)",
+                                               (tournament.tour_format, tournament.title, tournament.prize))
     tournament.id = generated_id
     return tournament
 
@@ -60,21 +62,14 @@ def manage_event(tournament, new_date, change_participants):
 def generate_game_schema(players):
     match_players = []
     players_count = len(players)
-    if players_count == 0:
+    if not players_count:
         return match_players
     if players_count % 2 != 0:
-        raise ValueError('Players must be even numbers')
-    if players_count == 2:
-        return 'final', {'first_player': players[0], 'second_player': players[1]}
+        raise BadRequest('Players must be even numbers')
     first_match_player1 = random.choice(players)
     players.remove(first_match_player1)
     first_match_player2 = random.choice(players)
     players.remove(first_match_player2)
-    second_match_player1 = random.choice(players)
-    players.remove(second_match_player1)
-    second_match_player2 = random.choice(players)
-    players.remove(second_match_player2)
-    match_players.append({'first_player': first_match_player1, 'second_player': first_match_player2})
     match_players.append({'first_player': first_match_player1, 'second_player': first_match_player2})
     match_players.extend(generate_game_schema(players))
     return match_players
@@ -91,7 +86,5 @@ def get_scheme_format(players_count):
         return 'Poveche nedavam'
 
 
-def tourn_exists(tourn_id: int) -> bool:
-    return any(
-        read_query(
-            '''SELECT 1 FROM tournaments WHERE id = ?''', (tourn_id,)))
+def tournament_exists(tour_title: str) -> bool:
+    return any(read_query('''SELECT 1 FROM tournaments WHERE title = ?''', (tour_title,)))
