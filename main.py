3d335815326067d543484c78e60b6c99de91fe8e
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-11-11 15:41:46 +0200

    main: + match_router

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from routers.user import users_router
 from routers.tournaments import tournaments_router
+from routers.matches import match_router
 import uvicorn
 
 app = FastAPI(debug=True)
 app.include_router(users_router)
 app.include_router(tournaments_router)
+app.include_router(match_router)
+
 if __name__ == "__main__":
     uvicorn.run('main:app', port=8000, reload=True)
