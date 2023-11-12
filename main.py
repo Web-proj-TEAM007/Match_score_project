4605256be3e789c100cdd8bc0acb1e10e9b5fbcd
Angel0v22w <velislavangelov19@gmail.com>
2023-11-12 19:30:55 +0200

    Admin router add to main file

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from routers.user import users_router
 from routers.tournaments import tournaments_router
 from routers.matches import match_router
+from routers.admin import admin_router
 import uvicorn
 
 app = FastAPI(debug=True)
 app.include_router(users_router)
 app.include_router(tournaments_router)
 app.include_router(match_router)
+app.include_router(admin_router)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', port=8000, reload=True)
