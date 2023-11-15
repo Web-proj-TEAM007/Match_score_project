4de1df9844da77a679bf748dd00185974e3453e3
Angel0v22w <velislavangelov19@gmail.com>
2023-11-15 11:01:10 +0200

    link_profile input update

--- routers/admin.py
+++ routers/admin.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Response
 from services import admin_service
 from authentication.jwt_bearer import JWTBearer
 from authentication.auth import get_user_from_token
+from data.models import Link_profile
 
 
 admin_router = APIRouter(prefix='/admin')
@@ -24,15 +25,14 @@ def director_promotion(user_id: int = None,
     return admin_service.handle(user_id, user.user_role)
 
 @admin_router.put('/requests/player')
-def link_profile(user_id: int = None, 
-                 player_id: int = None,
+def link_profile(data : Link_profile,
                  token: str = Depends(JWTBearer())):
     
     '''In order to link a user with a player profile you must put their id and the player profile id'''
     
     user = get_user_from_token(token)
 
-    return admin_service.link(user_id, player_id, user.user_role)
+    return admin_service.link(data.user_id, data.player_id, user.user_role)
 
 
     
