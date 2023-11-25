dee8a93380687d8c7d7f3d19cde196e26ad8ee41
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-11-25 18:24:41 +0200

    player_serv: updating opponents done

--- services/player_service.py
+++ services/player_service.py
@@ -68,23 +68,40 @@ def update_player_stat_tourn(player_id: int, t_win: bool) -> None | BadRequest:
     if not ans:
         raise BadRequest('Updating player tournament stats went wrong.')
 
-def upd_themost_played_opponent(player_id: int) -> None | BadRequest:
-
+def updating_player_opponents(player_id: int) -> None | BadRequest: 
+    
     ans = update_query('''UPDATE players_statistics 
-                            SET most_played_opp = (WITH matchess AS
+                    SET best_opp = (WITH matchess as
+                                    (SELECT matches_id FROM matches_has_players_profiles
+                                    WHERE player_profile_id = ?)
+                            SELECT pp.full_name 
+                            FROM matches_has_players_profiles mp, matchess ms, players_statistics ps, players_profiles pp
+                            WHERE mp.player_profile_id != ? and ms.matches_id = mp.matches_id 
+                                AND ps.player_profile_id = mp.player_profile_id and pp.id = mp.player_profile_id
+                            GROUP BY mp.player_profile_id
+                            ORDER BY (ps.matches_won/ps.matches_played) desc, pp.full_name
+                            LIMIT 1),
+                    worst_opp = (WITH matchesss as
+                                    (SELECT matches_id FROM matches_has_players_profiles
+                                    WHERE player_profile_id = ?)
+                            SELECT pp.full_name 
+                            FROM matches_has_players_profiles mp, matchesss ms, players_statistics ps, players_profiles pp
+                            WHERE mp.player_profile_id != ? and ms.matches_id = mp.matches_id 
+                                AND ps.player_profile_id = mp.player_profile_id and pp.id = mp.player_profile_id
+                            GROUP BY mp.player_profile_id
+                            ORDER BY (ps.matches_won/ps.matches_played) asc, pp.full_name
+                            LIMIT 1),
+                    most_played_opp = (WITH matchessss AS
                                                         (SELECT matches_id FROM matches_has_players_profiles
                                                         WHERE player_profile_id = ?)
                                     SELECT full_name FROM matches_has_players_profiles mp
                                     JOIN players_profiles pp ON pp.id = mp.player_profile_id
-                                    JOIN matchess m ON m.matches_id = mp.matches_id and mp.player_profile_id != ?
+                                    JOIN matchessss m ON m.matches_id = mp.matches_id and mp.player_profile_id != ?
                                     GROUP BY player_profile_id
                                     ORDER BY COUNT(mp.player_profile_id) desc, full_name desc
                                     LIMIT 1)
-                            WHERE player_profile_id = ?''', (player_id, player_id, player_id))
-    
+                    WHERE player_profile_id = ?''', (player_id, player_id, player_id, player_id, player_id, player_id, player_id))
+
     if not ans:
-        raise BadRequest('Updating the most played opponent went wrong.')
+        raise BadRequest('Updating player opponents went wrong.')
 
-def upd_thebest_opponent(player_id: int): pass
-
-def upd_theworst_opponent(player_id: int): pass
