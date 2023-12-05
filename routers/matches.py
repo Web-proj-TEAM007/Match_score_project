6f8fc67606c9d3d0896d9a7e0afc2178c4e168b0
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-12-05 21:41:06 +0200

    matches: new response format for the endpoint get_matches_by_tournament implemented

--- routers/matches.py
+++ routers/matches.py
@@ -49,7 +49,7 @@ def get_matches_by_tournament(tourn_id: int):
     if not ans:
         raise NotFound(f'Tournament #{tourn_id} not found.')
 
-    return match_service.get_matches_for_tournament(tourn_id)
+    return match_service.get_matches_by_tournament_v2(tourn_id)
 
 @match_router.put('/{match_id}', tags=['Matches'])
 def set_match_score(match_id: int, match_score: SetMatchScoreMod, token: str = Depends(JWTBearer())):
