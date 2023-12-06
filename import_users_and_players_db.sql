76aa7e9a5027686f0b515ef23e8c8206683bc4d5
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-12-06 20:40:15 +0200

    import users and players into the db

---
+++ import_users_and_players_db.sql
@@ -0,0 +1,12 @@
+-- users
+INSERT INTO matchscore_db.users(email, password, user_role) VALUES('director@test.bg', 'test', 'Director');
+INSERT INTO matchscore_db.users(email, password, user_role) VALUES('admin@test.bg', 'test', 'Admin');
+-- players
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('Kubrat Puevro', 'Bulgaria', 'BC Kobra');
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('John Johns', 'England', 'BC Johny');
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('Diego Ramirez', 'Argentina', 'BC  Boca Juniors');
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('Ahmed Al-Farsi', 'Saudi Arabia', 'BC  Al-Hilal');
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('Rafael Mendez', 'Spain', 'Real Club de Tenis Barcelona');
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('Andrei Petrov', 'Russia', 'Moscow Tennis Club');
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('Hiroshi Tanaka', 'Japan', 'Tokyo Lawn Tennis Club');
+INSERT INTO matchscore_db.players_profiles(full_name, country, club) VALUES('Fabio Rossi', 'Italy', 'Circolo Tennis Roma');
