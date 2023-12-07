e4604b4e0f72d50fafbfe3ae74dae04a191b6297
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-12-07 09:29:56 +0200

    player_serv_tests

--- tests/players_services_test.py
+++ tests/players_services_test.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import Mock, patch
 from services import player_service
 from data.models import PlayerStatistics
+from common.exceptions import BadRequest
 
 class PlayerService_Should(unittest.TestCase):
 
@@ -25,3 +26,53 @@ class PlayerService_Should(unittest.TestCase):
 
             # Assert
             self.assertEqual(expected.id, res.id)
+            self.assertEqual(expected.full_name, res.full_name)
+        
+    def test_GetPlayerByID_returns_None(self):
+
+        with patch('services.player_service.read_query') as read_query, \
+            patch('common.validators.form_ratio') as form_ratio_func:
+            # Arrange
+            read_query.return_value = []
+            form_ratio_func.return_value = []
+
+            # Act
+            res = player_service.get_player_by_id(1)
+
+            self.assertIsNone(res)
+    
+    def test_PlayerExists_ID_returns_True(self):
+        with patch('services.player_service.read_query') as read_query:
+
+            read_query.return_value = '1'
+
+            res = player_service.player_exists_id(1)
+
+            self.assertTrue(res)
+    
+    def test_PlayerExists_ID_returns_False(self):
+        with patch('services.player_service.read_query') as read_query:
+
+            read_query.return_value = ''
+
+            res = player_service.player_exists_id(1)
+
+            self.assertFalse(res)
+    
+    def test_update_player_stat_matches_return_None(self):
+        with patch('services.player_service.update_query') as update_query:
+
+            update_query.return_value = 1
+            res = player_service.update_player_stat_matches(1,True)
+
+            self.assertIsNone(res)
+    
+    # def test_update_player_stat_matches_return_BadRequest(self):
+    #     with patch('services.player_service.update_query') as update_query:
+
+    #         update_query.return_value = 0
+    #         res = player_service.update_player_stat_matches(1,True)
+
+    #         self.assertRaises(BadRequest, res)
+
+
