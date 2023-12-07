9ede92fc8880cfca28201af9a01f9126ca579422
Shahin0Sh <shahin.shukriev@hotmail.com>
2023-12-07 14:15:36 +0200

    player_serv: fix and better readability of upd opponents

--- services/player_service.py
+++ services/player_service.py
@@ -81,39 +81,9 @@ def update_player_stat_tourn(player_id: int, t_win: bool) -> None | BadRequest:
 
 def updating_player_opponents(player_id: int) -> None: 
     
-    update_query('''UPDATE players_statistics 
-                    SET best_opp = (WITH matchess as
-                                    (SELECT matches_id FROM matches_has_players_profiles
-                                    WHERE player_profile_id = ?)
-                            SELECT pp.full_name 
-                            FROM matches_has_players_profiles mp, matchess ms, players_statistics ps, players_profiles pp
-                            WHERE mp.player_profile_id != ? and ms.matches_id = mp.matches_id 
-                                AND ps.player_profile_id = mp.player_profile_id and pp.id = mp.player_profile_id
-                            GROUP BY mp.player_profile_id
-                            ORDER BY (CASE WHEN ps.matches_played > 0 THEN (ps.matches_won/ps.matches_played) ELSE 0 
-                            END) desc, pp.full_name
-                            LIMIT 1),
-                    worst_opp = (WITH matchesss as
-                                    (SELECT matches_id FROM matches_has_players_profiles
-                                    WHERE player_profile_id = ?)
-                            SELECT pp.full_name 
-                            FROM matches_has_players_profiles mp, matchesss ms, players_statistics ps, players_profiles pp
-                            WHERE mp.player_profile_id != ? and ms.matches_id = mp.matches_id 
-                                AND ps.player_profile_id = mp.player_profile_id and pp.id = mp.player_profile_id
-                            GROUP BY mp.player_profile_id
-                            ORDER BY (CASE WHEN ps.matches_played > 0 THEN (ps.matches_won/ps.matches_played) ELSE 0 
-                            END) asc, pp.full_name
-                            LIMIT 1),
-                    most_played_opp = (WITH matchessss AS
-                                                (SELECT matches_id FROM matches_has_players_profiles
-                                                WHERE player_profile_id = ?)
-                            SELECT full_name FROM matches_has_players_profiles mp
-                            JOIN players_profiles pp ON pp.id = mp.player_profile_id
-                            JOIN matchessss m ON m.matches_id = mp.matches_id and mp.player_profile_id != ?
-                            GROUP BY player_profile_id
-                            ORDER BY COUNT(mp.player_profile_id) desc, full_name desc
-                            LIMIT 1)
-                    WHERE player_profile_id = ?''', (player_id, player_id, player_id, player_id, player_id, player_id, player_id))
+    upd_most_played_opp(player_id)
+    upd_best_played_opp(player_id)
+    upd_worst_played_opp(player_id)
 
 
 def get_match_winner(match_id: int) -> list[int] | None:
@@ -159,3 +129,49 @@ def check_if_player_has_stats(player_id: int) -> bool:
 def creat_pl_stat(player_id: int) -> None:
 
     insert_query("INSERT INTO players_statistics(player_profile_id) VALUES(?)", (player_id,))
+
+def upd_most_played_opp(player_id: int) -> None:
+
+    update_query(''' UPDATE players_statistics
+                    SET most_played_opp = (WITH matchessss AS
+                                                (SELECT matches_id FROM matches_has_players_profiles
+                                                WHERE player_profile_id = ?)
+                            SELECT full_name FROM matches_has_players_profiles mp
+                            JOIN players_profiles pp ON pp.id = mp.player_profile_id
+                            JOIN matchessss m ON m.matches_id = mp.matches_id and mp.player_profile_id != ?
+                            GROUP BY player_profile_id
+                            ORDER BY COUNT(mp.player_profile_id) desc, full_name desc
+                            LIMIT 1)
+                    WHERE player_profile_id = ?''', (player_id, player_id, player_id))
+
+def upd_best_played_opp(player_id: int) -> None:
+
+    update_query('''UPDATE players_statistics
+                 SET best_opp = (WITH matchess as
+                                    (SELECT matches_id FROM matches_has_players_profiles
+                                    WHERE player_profile_id = ?)
+                            SELECT pp.full_name 
+                            FROM matches_has_players_profiles mp, matchess ms, players_statistics ps, players_profiles pp
+                            WHERE mp.player_profile_id != ? and ms.matches_id = mp.matches_id 
+                                AND ps.player_profile_id = mp.player_profile_id and pp.id = mp.player_profile_id
+                            GROUP BY mp.player_profile_id
+                            ORDER BY (CASE WHEN ps.matches_played > 0 THEN (ps.matches_won/ps.matches_played) ELSE 0 
+                            END) desc, pp.full_name
+                            LIMIT 1)
+                 WHERE player_profile_id = ?''', (player_id, player_id, player_id))
+
+def upd_worst_played_opp(player_id: int) -> None:
+
+    update_query('''UPDATE players_statistics 
+                 SET worst_opp = (WITH matchesss as
+                                    (SELECT matches_id FROM matches_has_players_profiles
+                                    WHERE player_profile_id = ?)
+                            SELECT pp.full_name 
+                            FROM matches_has_players_profiles mp, matchesss ms, players_statistics ps, players_profiles pp
+                            WHERE mp.player_profile_id != ? and ms.matches_id = mp.matches_id 
+                                AND ps.player_profile_id = mp.player_profile_id and pp.id = mp.player_profile_id
+                            GROUP BY mp.player_profile_id
+                            ORDER BY (CASE WHEN ps.matches_played > 0 THEN (ps.matches_won/ps.matches_played) ELSE 0 
+                            END) asc, pp.full_name
+                            LIMIT 1)
+                 WHERE player_profile_id = ?''', (player_id, player_id, player_id))
